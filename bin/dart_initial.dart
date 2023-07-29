8468958615f4fdfa0682dbe73615586b2ca3f54a
Henrique Rocha <almeida89henrique@gmail.com>
2023-07-29 15:07:06 +0000

    create project

---
+++ bin/dart_initial.dart
@@ -0,0 +1,5 @@
+import 'package:dart_initial/dart_initial.dart' as dart_initial;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_initial.calculate()}!');
+}
---
+++ test/dart_initial_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_initial/dart_initial.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
